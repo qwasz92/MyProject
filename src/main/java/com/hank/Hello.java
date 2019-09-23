18c97ed6c6097f45581c283b417773cb39bfd656
Hank Tom <lovevul35j6@gmail.com>
2019-09-23 16:01:28 +0800

    教學

--- src/main/java/com/hank/Hello.java
+++ src/main/java/com/hank/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello world");
         Person p =new Person();
         p.hello();
+        int age = 19;   //變數(全都小寫)
+        Integer age2 = 19;//(物件)
+        char c = 'A';   //字元
+        Character c2 ='A';
+        byte b = 120;
+        float weight =66.5f;
+        double height = 1.7;//精確值
+        boolean adult = true;//真假
+        String name = "Tom";//字串(大寫)
+
     }
 }
--- src/main/kotlin/com/hank/Hello.kt
+++ src/main/kotlin/com/hank/Hello.kt
@@ -5,6 +5,12 @@ fun main() {
 //    Human().hello()
     val h=Human()
     h.hello()
+    val age = 19//(無法改變)\\
+    var age2 = 18//(可改變)\\
+    age2 = 20
+    var weight = 66.5f
+    val name = "Hank"
+    //val name: String name="Hank"\\
 }
 class Human{
     fun hello(){
